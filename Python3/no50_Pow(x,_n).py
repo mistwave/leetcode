d1d44ada5a02815ab5687b4276563b33d298e362
Yifu D <duanyifu@hotmail.com>
2016-05-13 23:39:27 +0800

    debug no50 myPow

--- Python3/no50_Pow(x,_n).py
+++ Python3/no50_Pow(x,_n).py
@@ -8,13 +8,16 @@ class Solution(object):
         """
         if n == 0:
             return 1
-        elif x == 0 and n != 0:
+        if x == 0:
             return 0
-        elif n == 1:
-            return x
-        elif n % 2 == 0:
-            return self.myPow(x * x, n / 2)
-        elif n % 2 == 1:
-            return x * self.myPow(x, n - 1)
-        else:
-            return None
+        elif n > 0:
+            if n == 1:
+                return x
+            elif n % 2 == 0:
+                return self.myPow(x * x, n / 2)
+            elif n % 2 == 1:
+                return x * self.myPow(x, n - 1)
+            else:
+                return None
+        else:  # n < 0
+            return 1 / self.myPow(x, -n)
