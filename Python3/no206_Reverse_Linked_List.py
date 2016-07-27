254c1c6d3553ac5a62968d1f0f11f510000092fd
Yifu D <duanyifu@hotmail.com>
2016-07-27 21:22:12 +0800

    no206 reverse linked list recursive solution

---
+++ Python3/no206_Reverse_Linked_List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    # recursive solution
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        second = head.next
+        rest = self.reverseList(second)
+        second.next = head
+        head.next = None
+        return rest
+
+
