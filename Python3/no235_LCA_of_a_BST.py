e107f138ee7d32522f8ce376db02186c938bfeb0
Yifu Duan <duanyifu@hotmail.com>
2016-08-01 05:08:59 +0800

    3 ways for LCA of BST

--- Python3/no235_LCA_of_a_BST.py
+++ Python3/no235_LCA_of_a_BST.py
@@ -8,7 +8,7 @@
 
 class Solution(object):
 
-    def lowestCommonAncestor(self, root, p, q):
+    def lowestCommonAncestor_recursively(self, root, p, q):
         """
         :type root: TreeNode
         :type p: TreeNode
@@ -16,6 +16,7 @@ class Solution(object):
         :rtype: TreeNode
         """
         # to search for p or q
+        # 204ms
         if root in (None, p, q):
             return root
         left = self.lowestCommonAncestor(root.left, p, q)
@@ -29,3 +30,47 @@ class Solution(object):
         #     return root
         # else:
         #     return None
+
+        # brilliant solution for BST
+        # 140ms
+    def lowestCommonAncestor_BSTway(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        ans = root
+        while ans:
+            if min(p.val, q.val) > ans.val and ans.right:
+                ans = ans.right
+                continue
+            elif max(p.val, q.val) < ans.val and ans.left:
+                ans = ans.left
+                continue
+            return ans
+
+        # 116ms
+    def lowestCommonAncestor_pathway(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        def path(root, p):
+            s = []
+            now = root
+            while now and now.val != p.val:
+                s.append(now)
+                now = now.left if now.val > p.val else now.right
+            s.append(now)
+            return s
+
+        pathP, pathQ = path(root, p), path(root, q)
+        ans, i = None, 0
+        minlen = min(len(pathP), len(pathQ))
+        while i < minlen and pathP[i] == pathQ[i]:
+            ans = pathP[i]
+            i += 1
+        return ans
