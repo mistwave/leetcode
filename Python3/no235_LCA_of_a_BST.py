6ea34bf907791aefb7f96f8e538eef147c3c7f92
Yifu Duan <duanyifu@hotmail.com>
2016-08-01 04:25:24 +0800

    no235 LCA for BST

---
+++ Python3/no235_LCA_of_a_BST.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # to search for p or q
+        if root in (None, p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
+        # if left is None and right:
+        #     return right
+        # elif right is None and left:
+        #     return left
+        # elif right and left: # one node is in left and another is in right
+        #     return root
+        # else:
+        #     return None
